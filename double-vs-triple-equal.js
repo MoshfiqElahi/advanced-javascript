b18a613b25ba678a7c69b9392dafcc0f0f549a57
MoshfiqElahi <contact.moshfiq@gmail.com>
2020-08-07 10:40:58 +0600

    map-filter-find added

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,37 @@
+// const first =2;
+// const second = 3;
+// if(first == second){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// } it will say False
+
+//number vs STRING
+// const first =2;
+// const second = "2";
+// if(first == second){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// } it will say True   for interview- I will say- Double equal will check VALUE just
+
+// const first =2;
+// const second = "2";
+// if(first === second){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// } //it will say False  for interview- I will say- Triple equal will check VALUE and TYPE both.
+
+const first =1;
+const second = true;
+if(first == second){
+    console.log("True")
+}     // If I use == it will say TRUE. Because JS take 1 for the value of TRUE and 0 for the value of FALSE. If I use ===, it will say false because of TYPE. Triple equal will check VALUE and TYPE both.
+
+else{
+    console.log("False")
+}
---
+++ map-filter.js
@@ -0,0 +1,34 @@
+const numbers = [10, 3, 7, 13, 6, 4, 2, 5]
+// const output = [];
+
+// for(let i=0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result)
+// }
+// console.log(output)
+
+//MAP
+numbers.map(function (element, index, array) {
+    console.log(element, index, array) //By MAP We can see this 3 item!
+})
+
+//another system to make SQUARE!
+const result = numbers.map(function (element) {
+    return element * element;
+})
+console.log(result)
+
+//easy and fancy 
+const resultFancy = numbers.map(x => x * x)
+console.log(resultFancy)
+
+//filter
+const bigger = numbers.filter(x => x < 5)
+console.log(bigger)
+
+//search google about FOR EACH and Radios
+
+//find
+const isThere = numbers.find(x => x > 5); //it will ans the 1st bigger number and it is the smart version of FILTER
+console.log(isThere)
