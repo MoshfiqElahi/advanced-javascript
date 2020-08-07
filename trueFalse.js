1ea8510c26295db9dbb431510390f92af1b2343a
MoshfiqElahi <contact.moshfiq@gmail.com>
2020-08-07 09:18:01 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,24 @@
+//falsy:
+//0 
+//""
+//NaN
+//undefined
+// null
+//false
+
+//Truthy
+// "0"
+// " "
+//{}
+//[]
+//"false" //if you write false in a quotation it will take it as a true
+
+
+let name = NaN
+console.log(name)
+if (name) {  //if (name || name == 0) //write it if u want to get 0 as a true
+    console.log("True");
+}
+else {
+console.log("false")
+}
